2764c77e025805eda38ce35d33559d6080e4c98c
aibenshinto <aibenshinto@gmail.com>
2025-01-23 10:43:47 +0530

    created coupon html

--- coupons/views.py
+++ coupons/views.py
@@ -9,6 +9,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 
 # Vendor Registration API
@@ -25,8 +27,17 @@ class VendorRegisterAPI(generics.CreateAPIView):
 
 
 # Vendor Login API using JWT (Obtain Token Pair)
-class VendorLoginAPI(TokenObtainPairView):
-    pass
+class VendorLoginAPI(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
 
 
 # API to Create Coupons
